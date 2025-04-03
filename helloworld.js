3c387f08590cfe9c5cc8638cd72e6799acbbcb8d
izaias gomes <izaiasgomesdeoliveiraneto@gmail.com>
2025-04-03 16:35:48 -0400

    change

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,19 @@
 //primeiro hello world
-var valor=20.13
+
+var valor=20.13465456
+
 var text= 'primeiro projeto javascript'
+
 var ativo= true
 
 console.log (valor.toFixed(2));//arredonda o valor para duas casas decimais
+
 console.log(text.toUpperCase());//converte o texto para letras maisculas
+
 console.log(ativo.toString());//converte para texto
+
+console.log("tipo " + typeof text);//typeof exibe o tipo da variavel
+
+console.log("tipo " + typeof ativo);//typeof exibe o tipo da variavel
+
+console.log("tipo " + typeof valor);//typeof exibe o tipo da variavel
