3fba82fded4561062a41c7d7027e186bbc0fbeef
izaias gomes <izaiasgomesdeoliveiraneto@gmail.com>
2025-04-03 16:25:29 -0400

    first code

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,8 @@
 //primeiro hello world
-console.log ('hello world');
+var valor=20.13
+var text= 'primeiro projeto javascript'
+var ativo= true
+
+console.log (valor.toFixed(2));//arredonda o valor para duas casas decimais
+console.log(text.toUpperCase());//converte o texto para letras maisculas
+console.log(ativo.toString());//converte para texto
